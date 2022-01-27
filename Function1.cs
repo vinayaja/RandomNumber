2f13d3f6a71fd71a901ea0dfccb28b494ceb9e10
vinayaja <94535778+vinayaja@users.noreply.github.com>
2022-01-27 01:25:57 +0000

    Add files via upload

---
+++ Function1.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace RandomNumber
+{
+    public static class GetRandomNumber
+    {
+        [FunctionName("GetRandomNumber")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            int min = 1;
+            int max = 100;
+
+            Random random = new Random();
+            int number = random.Next(min, max);
+
+            return new OkObjectResult(number);
+        }
+    }
+}
